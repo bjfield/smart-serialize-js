d381a0ab1a3a03f5adb224f3863dfaf7c5e2498f
Ben <bfield@newrelic.com>
2023-04-23 19:45:59 -0700

    Improve handling of edge cases; add tests

--- index.js
+++ index.js
@@ -9,33 +9,17 @@
 function smartSerialize (object, maxDepth = Infinity, maxSize = Infinity) {
   const seen = new WeakSet()
 
-  let output = ''
-  function isBraceOrBracket (value) {
-    return value === '[' || value === ']' || value === '[]' || value === '{' || value === '}' || value === '{}'
-  }
-  function render (key, value, index) {
-    let str = ''
-    if (index) str = ','
-    if (key) str += '"' + key + '":'
-    if (isBraceOrBracket(value)) {
-      str += value
-    } else if (typeof value === 'string') {
-      str += '"' + value + '"'
-    } else str += value
-    output = str + output
-  }
-
   let size = 0
   let sizeLimitReached = false
   function measureSize (value) {
     if (value === null || value === true) size += 4
     else if (value === false) size += 5
-    else if (typeof value === 'string') size += value.length + 2 // + ""
-    else if (typeof value === 'number') size += String(value).length
+    else if (typeof value === 'string' || value instanceof String) size += value.length + 2 // + ""; not accounting for escape characters
+    else if (typeof value === 'number' || value instanceof Number) size += String(value).length
     else if (value instanceof Date) size += 26 // ISO format + ""
-    if (size > maxSize) {
-      sizeLimitReached = true
-    }
+    else if (value instanceof Error || value instanceof RegExp) size += value.toString()
+    else if (typeof value === 'function') size += 13 + value.name.length // "[function ${value.name}]"
+    if (size > maxSize) sizeLimitReached = true
   }
   function addSize (strLength) {
     size += strLength
@@ -44,40 +28,116 @@ function smartSerialize (object, maxDepth = Infinity, maxSize = Infinity) {
     }
   }
 
+  /*
+   * These symbols are used to create mock opening and closing nodes for arrays and objects on the stack.
+   * We can't put simple string values like '[' on the stack because the serialized object might contain these.
+   */
+  const LEFT_BRACKET = Symbol('[')
+  const RIGHT_BRACKET = Symbol(']')
+  const BRACKETS = Symbol('[]')
+  const LEFT_BRACE = Symbol('{')
+  const RIGHT_BRACE = Symbol('}')
+  const BRACES = Symbol('{}')
+
+  const enclosures = [LEFT_BRACKET, RIGHT_BRACKET, BRACKETS, LEFT_BRACE, RIGHT_BRACE, BRACES]
+
+  const getEnclosure = (value) => {
+    switch (value) {
+      case LEFT_BRACKET: return '['
+      case RIGHT_BRACKET: return ']'
+      case BRACKETS: return '[]'
+      case LEFT_BRACE: return '{'
+      case RIGHT_BRACE: return '}'
+      case BRACES: return '{}'
+    }
+  }
+
+  let output = ''
+  function render (key, value, index) {
+    let str = ''
+    if (index) str = ','
+    if (key) str += '"' + key + '":'
+    if (enclosures.includes(value)) {
+      str += getEnclosure(value)
+    } else if (typeof value === 'string' || value instanceof String) {
+      str += '"' + escape(value) + '"'
+    } else str += value
+    output = str + output
+  }
+
+  /**
+   * @see {@link https://github.com/bestiejs/json3/blob/master/lib/json3.js#L419 | JSON3 implementation}
+   */
+  // eslint-disable-next-line no-control-regex, no-misleading-character-class
+  const ESCAPE_REGEXP = /[\x00-\x1f\x22\x5c]/g // ASCII: control characters 0-31; quote character; backslash
+
+  /**
+   * Escapes any characters that would create a problematic string representation.
+   * @see {@link https://github.com/douglascrockford/JSON-js/blob/master/json2.js#L215-L231 | JSON2 quote function}
+   *
+   * @param {string} value - A string to be checked for problem characters and escaped.
+   */
+  function escape (value) {
+    ESCAPE_REGEXP.lastIndex = 0 // always reset the index at which to start the next match
+    if (ESCAPE_REGEXP.test(value)) {
+      return value.replaceAll(ESCAPE_REGEXP, function (a) {
+        const c = {
+          '\b': '\\b',
+          '\t': '\\t',
+          '\n': '\\n',
+          '\f': '\\f',
+          '\r': '\\r',
+          '"': '\\"',
+          '\\': '\\\\'
+        }[a]
+        if (typeof c === 'string') return c
+        return '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4)
+      })
+    }
+    return value
+  }
+
   const stack = [[null, object, undefined, 0]] // key, value, index, depth
 
   while (stack.length) {
     const [key, value, index, depth] = stack.pop()
-    if (typeof value === 'object' && !seen.has(value)) {
+    if (seen.has(value)) {
+      addSize(20) // [circular reference]
+      if (!sizeLimitReached) render(key, '[circular reference]', index)
+    } else if (typeof value === 'function') {
+      render(key, `[function ${value.name || '(anonymous)'}]`, index, depth)
+    } else if (value !== null && typeof value === 'object') {
       seen.add(value)
       if (Array.isArray(value)) {
         addSize(2) // []
         if (!sizeLimitReached && depth < maxDepth) {
-          stack.push([key, '[', index, depth])
+          stack.push([key, LEFT_BRACKET, index, depth])
           for (let i = 0; i < value.length; i++) {
             measureSize(value[i])
             if (!sizeLimitReached) stack.push([null, value[i], i, depth + 1])
           }
-          stack.push([undefined, ']', undefined, depth])
+          stack.push([undefined, RIGHT_BRACKET, undefined, depth])
         } else {
-          render(key, '[]', index)
+          render(key, BRACKETS, index)
         }
       } else if (value instanceof Date) {
         render(key, value.toISOString(), index, depth)
+      } else if (value instanceof Error || value instanceof RegExp) {
+        render(key, value.toString(), index, depth)
       } else {
         const keys = Object.keys(value)
         if (keys.length) {
           addSize(2) // {}
           if (!sizeLimitReached && depth < maxDepth) {
-            stack.push([key, '{', index, depth])
+            stack.push([key, LEFT_BRACE, index, depth])
             keys.forEach((k, i) => {
               addSize(k.length + 3) // "":
               if (!sizeLimitReached) measureSize(value[k])
               if (!sizeLimitReached) stack.push([k, value[k], i, depth + 1])
             })
-            stack.push([undefined, '}', undefined, depth])
+            stack.push([undefined, RIGHT_BRACE, undefined, depth])
           } else {
-            render(key, '{}', index)
+            render(key, BRACES, index)
           }
         }
       }
--- index.test.js
+++ index.test.js
@@ -12,10 +12,14 @@ describe('smartSerialize', () => {
     expect(result).toEqual('{"foo":1,"bar":"baz"}')
   })
 
-  it('serializes a nested object', () => {
-    const obj = { foo: { bar: { baz: 'qux' } } }
-    const result = smartSerialize(obj)
-    expect(result).toEqual('{"foo":{"bar":{"baz":"qux"}}}')
+  test('serializes an object with a nested object value', () => {
+    const obj = { foo: { bar: 'baz' } }
+    expect(smartSerialize(obj)).toBe('{"foo":{"bar":"baz"}}')
+  })
+
+  test('serializes an object with a nested array value', () => {
+    const obj = { foo: ['bar', 'baz'] }
+    expect(smartSerialize(obj)).toBe('{"foo":["bar","baz"]}')
   })
 
   it('serializes arrays', () => {
@@ -36,18 +40,62 @@ describe('smartSerialize', () => {
     expect(result).toEqual('{}')
   })
 
-  it('serializes a date object', () => {
+  it('serializes a Date object', () => {
     const obj = { date: new Date('2022-01-01T00:00:00.000Z') }
     const result = smartSerialize(obj)
     expect(result).toEqual('{"date":"2022-01-01T00:00:00.000Z"}')
   })
-})
-test('serializes object with both maxDepth and maxSize', () => {
-  const input = ['0123456789', { a: 1, b: 2 }, { n: { n: { n: { n: { n: { n: { n: {} } } } } } } }, 12.345, { longLongLongKey: 0 }]
-  const expectedOutputLength = 67
-  const maxSize = 70
-  const maxDepth = 4
 
-  const output = smartSerialize(input, maxDepth, maxSize)
-  expect(output).toHaveLength(expectedOutputLength)
+  test('serializes object with both maxDepth and maxSize', () => {
+    const input = ['0123456789', { a: 1, b: 2 }, { n: { n: { n: { n: { n: { n: { n: {} } } } } } } }, 12.345, { longLongLongKey: 0 }]
+    const expectedOutputLength = 67
+    const maxSize = 70
+    const maxDepth = 4
+
+    const output = smartSerialize(input, maxDepth, maxSize)
+    expect(output).toHaveLength(expectedOutputLength)
+  })
+
+  test('serializes an object with a number value', () => {
+    const obj = { foo: 42 }
+    expect(smartSerialize(obj)).toBe('{"foo":42}')
+  })
+
+  test('serializes an object with a boolean value', () => {
+    const obj = { foo: true }
+    expect(smartSerialize(obj)).toBe('{"foo":true}')
+  })
+
+  test('serializes an object with a null value', () => {
+    const obj = { foo: null }
+    expect(smartSerialize(obj)).toBe('{"foo":null}')
+  })
+
+  test('serializes an object with an Error value', () => {
+    const obj = { foo: new Error('error message') }
+    expect(smartSerialize(obj)).toBe('{"foo":"Error: error message"}')
+  })
+
+  test('serializes an object with a RegExp value', () => {
+    const obj = { foo: /hello/i }
+    expect(smartSerialize(obj)).toBe('{"foo":"/hello/i"}')
+  })
+
+  test('should handle circular references in objects', () => {
+    const obj = { foo: {} }
+    obj.foo.bar = obj
+    expect(smartSerialize(obj)).toBe('{"foo":{"bar":"[circular reference]"}}')
+  })
+
+  test('properly escapes special characters', () => {
+    const input = {
+      tab: '\t',
+      lineFeed: '\n',
+      doubleQuote: '"',
+      singleQuote: "'",
+      backslash: '\\'
+    }
+    const expectedOutput = '{"tab":"\\t","lineFeed":"\\n","doubleQuote":"\\"","singleQuote":"\'","backslash":"\\\\"}'
+    expect(smartSerialize(input)).toEqual(expectedOutput)
+  })
 })
